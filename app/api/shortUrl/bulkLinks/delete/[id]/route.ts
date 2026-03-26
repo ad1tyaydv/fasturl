66d751b28861e2e5fff011d638559c82ef656530
Aditya Yadav <adityaydv12890@gmail.com>
2026-03-26 19:27:23 +0000

    again simple fix

--- app/api/shortUrl/bulkLinks/delete/[id]/route.ts
+++ app/api/shortUrl/bulkLinks/delete/[id]/route.ts
@@ -2,9 +2,9 @@ import { prisma } from "@/lib/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function POST({ params }: {params: Promise<{ id: string }> }) {
+export async function POST(req: NextRequest, context : {params: Promise<{ id: string }> }) {
 
-    const { id } = await params;
+    const { id } = await context.params;
 
     try {
 
