f4bb44ba0aae61ee8fc6a22ae4b0986b8e9ef4eb
Aditya Yadav <adityaydv12890@gmail.com>
2026-03-27 15:41:06 +0000

    redis added

--- app/[shortUrl]/route.ts
+++ app/[shortUrl]/route.ts
@@ -2,27 +2,31 @@ import { prisma } from "@/lib/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 import { stateMap } from "../helpers/getStateName";
 import { countryMap } from "../helpers/getCountryName";
-
+import { redis } from "@/lib/redis";
 
 const ANON_USER_CLICK = process.env.ANONYMOUS_USER_CLICK!;
 
 export async function GET(req: NextRequest, { params }: { params: Promise<{ shortUrl: string }> }) {
-
+    
     const { shortUrl } = await params;
 
-    const ip = req.headers.get("x-forwarded-for")?.split(",")[0] ||
+    const ip =
+        req.headers.get("x-forwarded-for")?.split(",")[0] ||
         req.headers.get("x-real-ip") ||
         "127.0.0.1";
 
     const country = req.headers.get("x-vercel-ip-country") || "Unknown";
     const countryFullName = countryMap[country] ?? country;
 
-    const state = req.headers.get("x-vercel-ip-country-region") || "Unknown";
+    const state =
+        req.headers.get("x-vercel-ip-country-region") || "Unknown";
     const stateFullName = stateMap[state] ?? state;
 
     const city = req.headers.get("x-vercel-ip-city") || "Unknown";
 
-    let getBrowser = req.headers.get("sec-ch-ua")?.replace(/"/g, "");
+    let getBrowser = req.headers
+        .get("sec-ch-ua")
+        ?.replace(/"/g, "");
     let browser = "Unknown";
 
     if (getBrowser) {
@@ -36,70 +40,87 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{ shor
     const deviceCheck = req.headers.get("sec-ch-ua-mobile");
     const device = deviceCheck === "?1" ? "Mobile" : "Desktop";
 
-    const operatingSystem = req.headers.get("sec-ch-ua-platform");
+    const operatingSystem =
+        req.headers.get("sec-ch-ua-platform");
 
     let ref = req.headers.get("referer");
     let referrer = "Direct";
 
     if (ref) {
         try {
-            const domain = new URL(ref).hostname.toLowerCase();
+        const domain = new URL(ref).hostname.toLowerCase();
 
-            if (domain.includes("twitter")) referrer = "Twitter";
-            else if (domain.includes("facebook")) referrer = "Facebook";
-            else if (domain.includes("linkedin")) referrer = "LinkedIn";
-            else if (domain.includes("instagram")) referrer = "Instagram";
-            else if (domain.includes("google")) referrer = "Google";
-            else referrer = domain;
-
-        } catch {
-
-        }
+        if (domain.includes("twitter")) referrer = "Twitter";
+        else if (domain.includes("facebook"))
+            referrer = "Facebook";
+        else if (domain.includes("linkedin"))
+            referrer = "LinkedIn";
+        else if (domain.includes("instagram"))
+            referrer = "Instagram";
+        else if (domain.includes("google"))
+            referrer = "Google";
+        else referrer = domain;
+        } catch {}
     }
 
+
+    const cachedUrl = await redis.get(`link:${shortUrl}`);
 
     const findUrl = await prisma.link.findUnique({
         where: {
-            shorturl: shortUrl
-        }
-    })
+        shorturl: shortUrl,
+        },
+    });
 
     if (!findUrl) {
         return NextResponse.json(
-            { message: "URL not found" },
-            { status: 404 }
-        )
+        { message: "URL not found" },
+        { status: 404 }
+        );
     }
 
-    if(findUrl.password) {
-        return NextResponse.redirect(new URL(`/verify/${shortUrl}`, req.url));
+    if (!cachedUrl) {
+        await redis.set(
+        `link:${shortUrl}`,
+        findUrl.original,
+        {
+            ex: 60 * 60 * 24,
+        }
+        );
+    }
+
+    if (findUrl.password) {
+        return NextResponse.redirect(
+        new URL(`/verify/${shortUrl}`, req.url)
+        );
     }
 
     await prisma.link.update({
         where: {
-            shorturl: shortUrl
+        shorturl: shortUrl,
         },
         data: {
-            clicks: {
-                increment: 1
-            }
-        }
-    }),
+        clicks: {
+            increment: 1,
+        },
+        },
+    });
 
     await prisma.click.create({
         data: {
-            linkId: findUrl.id,
-            ip,
-            country: countryFullName,
-            state: stateFullName,
-            city,
-            browser,
-            device,
-            OS: operatingSystem,
-            referrer
-        }
-    })
+        linkId: findUrl.id,
+        ip,
+        country: countryFullName,
+        state: stateFullName,
+        city,
+        browser,
+        device,
+        OS: operatingSystem,
+        referrer,
+        },
+    });
 
-    return NextResponse.redirect(findUrl.original);
-    
+    return NextResponse.redirect(
+        (cachedUrl as string) || findUrl.original
+    );
 }
---
+++ lib/redis.ts
@@ -0,0 +1,6 @@
+import { Redis } from '@upstash/redis'
+
+export const redis = Redis.fromEnv()
+
+await redis.set("foo", "bar");
+await redis.get("foo");
