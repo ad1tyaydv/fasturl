e24ccb2ad4798aa3d259f86abfc0935788b3f250
Aditya Yadav <adityaydv12890@gmail.com>
2026-03-27 12:37:51 +0000

    typescript error fixed

--- app/api/shortUrl/bulkLinks/delete/[id]/route.ts
+++ app/api/shortUrl/bulkLinks/delete/[id]/route.ts
@@ -1,28 +1,32 @@
 import { prisma } from "@/lib/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function POST(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
 
-export async function DELETE(req: NextRequest, { params } : {params: { id: string } }) {
+  const { id } = await params;
 
-    const { id } = params;
+  try {
 
-    try {
+    await prisma.bulkLinks.delete({
+      where: {
+        id: id,
+      },
+    });
 
-        await prisma.bulkLinks.delete({
-            where: {
-                id: id,
-            }
-        })
+    return NextResponse.json(
+      {message: "Link deleted"},
+      {status: 200}
+    );
 
-        return NextResponse.json(
-            {message: "Bulk link deleted"},
-            {status: 200}
-        )
+  } catch (error) {
 
-    } catch (error) {
-        return NextResponse.json(
-            {message: "Error while deleting bulk links"},
-            {status: 500}
-        )
-    }
+    return NextResponse.json(
+      { message: "Error while deleting link" },
+      { status: 500 }
+    );
+
+  }
 }
--- app/api/shortUrl/delete/[id]/route.ts
+++ app/api/shortUrl/delete/[id]/route.ts
@@ -1,8 +1,8 @@
 import { prisma } from "@/lib/dbConfig";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 
-export async function POST({ params }: { params: Promise<{ id: string}> } ) {
+export async function POST(req: NextRequest, { params }: { params: Promise<{ id: string}> } ) {
 
     const { id } = await params;
 
