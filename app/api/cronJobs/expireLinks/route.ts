d3995739faa79b3b8441c84a17ccd0511adee22d
Aditya Yadav <adityaydv12890@gmail.com>
2026-04-05 05:44:38 +0000

    crons jobs expire link method updated

--- app/api/cronJobs/expireLinks/route.ts
+++ app/api/cronJobs/expireLinks/route.ts
@@ -1,45 +1,55 @@
 import { prisma } from "@/lib/dbConfig";
 import { redis } from "@/lib/redis";
-import cron from "node-cron";
+import { NextResponse } from "next/server";
 
 
-cron.schedule("*/5 * * * *", async () => {
-    try {
-        const now = new Date();
+export async function GET() {
+    
+  try {
+    const now = new Date();
 
-        const expiredLinks = await prisma.link.findMany({
-            where: {
-                expiresAt: { 
-                    lte: now
-                }
-            },
-            select: {
-                shorturl: true,
-                userId: true
-            }
-        });
+    const expiredLinks = await prisma.link.findMany({
+      where: {
+        expiresAt: {
+          lte: now
+        }
+      },
+      select: {
+        shorturl: true,
+        userId: true
+      }
+    });
 
-        const deleted = await prisma.link.deleteMany({
-            where: {
-                expiresAt: {
-                    lte: now
-                }
-            }
-        })
+    const deleted = await prisma.link.deleteMany({
+      where: {
+        expiresAt: {
+          lte: now
+        }
+      }
+    });
 
-        await Promise.all(
-            expiredLinks.map((link) =>
-                Promise.all([
-                    redis.del(`fetchUrls:${link.userId}`),
-                    redis.del(`analytics:${link.shorturl}`),
-                    redis.del(`link:${link.shorturl}`),
-                ])
-            )
-        )
+    await Promise.all(
+      expiredLinks.map((link) =>
+        Promise.all([
+          redis.del(`fetchUrls:${link.userId}`),
+          redis.del(`analytics:${link.shorturl}`),
+          redis.del(`link:${link.shorturl}`),
+        ])
+      )
+    );
 
-        console.log(`[CRON] Deleted ${deleted.count} expired links at ${new Date().toISOString()}`);
+    console.log(`[CRON] Deleted ${deleted.count} expired links`);
 
-    } catch (error) {
-        console.log("Error deleting expired Links", error);
-    }
-})
+    return NextResponse.json({
+      success: true,
+      deleted: deleted.count
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json({
+      success: false
+    });
+  }
+}
